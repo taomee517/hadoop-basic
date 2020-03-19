8e2eb9a3803d4e98f1f8725544d705772686e41e
Administrator <taomee517@gmail.com>
2020-03-19 19:17:01 +0800

    新增combiner案例

--- src/main/java/com/demo/hadoop/reducer/TerminalLogCountCombiner.java
+++ src/main/java/com/demo/hadoop/reducer/TerminalLogCountCombiner.java
@@ -6,14 +6,13 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class TerminalLogCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class TerminalLogCountCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
     Text outKey = new Text();
     IntWritable outValue = new IntWritable();
     private static int index = 0;
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        System.out.println("TerminalLogCountReducer被调用次数：" + index);
         index++;
         int count = 0;
 
